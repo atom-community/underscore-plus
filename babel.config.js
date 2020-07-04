7ec4f94e4cb9ff1e9488daa6aa413c46f45d20ec
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-07-04 05:47:19 -0500

    using babel

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+let presets = [];
+
+let plugins = [
+  ["@babel/plugin-proposal-optional-chaining", { loose: false }],
+  ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: false }],
+
+  "@babel/plugin-proposal-export-namespace-from",
+];
+
+if (process.env.BABEL_ENV === "cjs") {
+  plugins.push("@babel/plugin-transform-modules-commonjs");
+}
+
+module.exports = {
+  presets: presets,
+  plugins: plugins,
+  exclude: "node_modules/**",
+  sourceMap: true,
+};
